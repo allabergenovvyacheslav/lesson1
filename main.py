cc93ae6b4836a4d82738d5595f6a96fe93dee785
Vyacheslav <slawushkin@mail.ru>
2024-06-08 22:10:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+homework = 12
+spent_time = 1.5
+course_name = 'Python'
+time_for_one_homework = spent_time / homework
+print('Курс:', course_name,',' " всего задач:", homework ,',' " затрачено часов: ", spent_time ,
+',' " среднее время выполнения: " , time_for_one_homework)
